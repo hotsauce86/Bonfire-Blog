3adf4b0296d28f1de0459b7ecfc8baeb0e6f973a
hotsauce86 <timothy_tierney@my.uri.edu>
2019-09-03 13:56:02 -0400

    reworking form

--- addmovie.php
+++ addmovie.php
@@ -7,12 +7,22 @@
 
 	<p>Here we are going to add the details of a film and add it to our database</p>
 
+	<div>
 
-	<form action="">
-		<input type="string" name="title">
-		<input type="string" name="director">
-		<input type="int" name="year">
-	</form>
+		<form action="/example.php" method="POST">
+			Title: <br>
+			<input type="text" name="title"><br>
+			Director: <br>
+			<input type="text" name="director"><br>
+			Year: <br>
+			<input type="int" name="year"><br>
+			<input type="submit" name="Submit">
+		</form>
+
+
+	</div>
+
+	
 
 </body>
 </html>
