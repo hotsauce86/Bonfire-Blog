ca8b9b47bb8add666750a9c4b6242493ab16a127
hotsauce86 <timothy_tierney@my.uri.edu>
2019-09-02 21:49:59 -0400

    adding new project files to site

---
+++ addmovie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Movie</title>
+</head>
+<body>
+
+	<p>Here we are going to add the details of a film and add it to our database</p>
+
+
+	<form action="">
+		<input type="string" name="title">
+		<input type="string" name="director">
+		<input type="int" name="year">
+	</form>
+
+</body>
+</html>
---
+++ example.php
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>hello there</title>
+</head>
+<body>
+
+	<p>this is an example page with some php and js</p>
+
+	<p>in this section we are going to insert more movies into our database</p>
+
+
+	<?php
+
+	$title = $_POST['title'];
+	$director = $_POST['director'];
+	$year = $_POST['year'];
+
+	?>
+
+</body>
+</html>
